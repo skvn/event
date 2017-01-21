8b22e0ef8ab159a1427f9fe719c627694c14febf
serg <ohqncp>
2017-01-21 13:12:19 +0300

    -- new event system

--- Event.php
+++ Event.php
@@ -2,7 +2,36 @@
 
 namespace Skvn\Event;
 
-class Event implements Contracts\Event
+class Event implements Contracts\Event, \ArrayAccess
 {
+    protected $payload;
+
+    function __construct($payload = [])
+    {
+        $this->payload = $payload;
+    }
+
+    function offsetExists($offset)
+    {
+        return isset($this->payload[$offset]);
+    }
+
+    function offsetGet($offset)
+    {
+        return $this->payload[$offset] ?? null;
+    }
+
+    function offsetSet($offset, $value)
+    {
+        return false;
+    }
+
+    function offsetUnset($offset)
+    {
+        return false;
+    }
+
+
+
 
 }
