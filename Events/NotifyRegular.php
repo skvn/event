5cb040b235b2ac003f14a1180a9b3193417eb4ab
serg <ohqncp>
2017-03-31 06:18:09 +0300

    -- sync

---
+++ Events/NotifyRegular.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Skvn\Event\Events;
+
+use Skvn\Event\Event as BaseEvent;
+
+
+/**
+ * Class NotifyRegular
+ * @package Skvn\Event\Events
+ *
+ * @property \Skvn\App\Application $app
+ * @property string $subject
+ * @property string $message
+ */
+class NotifyRegular extends BaseEvent
+{
+
+}
