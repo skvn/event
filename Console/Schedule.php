aa8e11123a81c9428d70133d7c87ba5c133ba9f1
serg <kulyamin_sergey@mail.ru>
2017-04-26 19:14:23 +0300

    -- cron

--- Console/Schedule.php
+++ Console/Schedule.php
@@ -29,14 +29,46 @@ class Schedule extends ConsoleActionEvent
      */
     function actionRun()
     {
+        $t = microtime(true);
         $entries = $this->getScheduledEntries();
+        $this->app->triggerEvent(new Log(['message' => 'Commands fetched ' . round(microtime(true) - $t, 5), 'category' => 'cron/starter']));
         foreach ($entries as $entry) {
             $command = $this->buildCommand($entry);
             exec($command);
             $this->app->triggerEvent(new Log(['message' => $command  . '(' . microtime(true) . ')', 'category' => 'cron/start']));
         }
+        $this->app->triggerEvent(new Log(['message' => 'Commands executed ' . round(microtime(true) - $t, 5), 'category' => 'cron/starter']));
         $this->app->triggerEvent(new SchedulerDone());
 
+    }
+
+    /**
+     * Dump current crontabs scheme
+     */
+    function actionTabs()
+    {
+        $this->stdout('Dumping all tabs....');
+        $this->stdout('');
+        $this->stdout('');
+        $chost = null;
+
+        array_map(function($item) use (&$chost){
+            if ($chost != $item['host']) {
+                $this->stdout('');
+                $this->stdout('<bold>-----===== Dumping host ' . $item['host'] . ' =====-----</bold>');
+                $this->stdout('');
+                $chost = $item['host'];
+            }
+            $opts = [];
+            foreach ($item['options'] ?? [] as $k => $v) {
+                $opts[] = '--' . $k . '=' . (is_array($v) ? implode(',', $v) : $v);
+            }
+            $this->stdout($item['host'] . ': ' .
+                    str_pad($item['time']->getExpression(), 16, ' ') . ' ' .
+                    str_pad($item['user'], 7, ' ') . ' ' .
+                    '<cyan>' . (!empty($item['cmd']) ? $item['cmd'] : (Str :: snake(Str :: classBasename($item['command'])) . '/' . $item['action'])) . '</cyan> ' .
+                    implode(' ', $opts));
+        }, $this->getScheduledEntries(true));
     }
 
     protected function buildCommand($entry)
@@ -55,18 +87,44 @@ class Schedule extends ConsoleActionEvent
         return implode(' ', $cmd);
     }
 
-    protected function getScheduledEntries()
+    protected function getScheduledEntries($all = false)
     {
         $entries = [];
         foreach ($this->app->getAvailableCommands() as $command) {
             if ($command instanceof ScheduledEvent) {
                 foreach ($command->schedule() as $entry) {
-                    if ($entry['time']->isDue() && $this->app->filterScheduledEntry($entry)) {
+                    if ($all || $entry['time']->isDue() && $this->app->filterScheduledEntry($entry)) {
                         $entry['command'] = $command;
                         $entries[] = $entry;
                     }
                 }
             }
+        }
+        if ($all) {
+            $entries = $this->app->appendScheduledEntries($entries);
+            usort($entries, function($a, $b){
+                $at = explode(' ', $a['time']->getExpression());
+                $bt = explode(' ', $b['time']->getExpression());
+                $wa = $a['host']*1000000;
+                $wb = $b['host']*1000000;
+                if ($at[2] != '*') $wa += $at[2] * 100000;
+                if ($bt[2] != '*') $wb += $bt[2] * 100000;
+                if ($at[4] != '*') $wa += $at[4] * 10000;
+                if ($bt[4] != '*') $wb += $bt[4] * 10000;
+                if ($at[1] != '*') $wa += $at[1] * 60 + $at[0] + 100;
+                if ($bt[1] != '*') $wb += $bt[1] * 60 + $bt[0] + 100;
+                if (Str :: pos('*', $at[0]) === false) {
+                    $wa += 70 + $at[0];
+                } elseif ($at[0] !== '*') {
+                    $wa += intval(str_replace('*/', '', $at[0]));
+                }
+                if (Str :: pos('*', $bt[0]) === false) {
+                    $wb += 70 + $bt[0];
+                } elseif ($bt[0] !== '*') {
+                    $wb += intval(str_replace('*/', '', $bt[0]));
+                }
+                return $wa <=> $wb;
+            });
         }
         return $entries;
     }
