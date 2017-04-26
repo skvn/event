710101898d24a55d7f3f28a8f2a7b7501bc2895f
serg <kulyamin_sergey@mail.ru>
2017-04-26 12:48:03 +0300

    -- cron

--- Console/Schedule.php
+++ Console/Schedule.php
@@ -8,6 +8,7 @@ use Skvn\Base\Helpers\Str;
 use Skvn\Base\Exceptions\ConsoleException;
 use Skvn\Event\Contracts\ScheduledEvent;
 use Skvn\Event\Events\Log;
+use Skvn\Event\Events\SchedulerDone;
 
 
 /**
@@ -32,8 +33,9 @@ class Schedule extends ConsoleActionEvent
         foreach ($entries as $entry) {
             $command = $this->buildCommand($entry);
             exec($command);
-            $this->app->triggerEvent(new Log(['message' => $command, 'category' => 'cron/start']));
+            $this->app->triggerEvent(new Log(['message' => $command  . '(' . microtime(true) . ')', 'category' => 'cron/start']));
         }
+        $this->app->triggerEvent(new SchedulerDone());
 
     }
 
@@ -41,12 +43,15 @@ class Schedule extends ConsoleActionEvent
     {
         $cmd = [];
         if (!empty($entry['user']) && $entry['user'] != 'root') {
-            $cmd[] = 'sudo -u ' . $entry['user'];
+            $cmd[] = '/usr/local/bin/sudo -u ' . $entry['user'];
         }
         $cmd[] = PHP_BINARY;
         $cmd[] = $this->app->request->getServer('SCRIPT_NAME');
         $cmd[] = Str :: snake(Str :: classBasename($entry['command'])) . '/' . $entry['action'];
-        $cmd[] = '2>&1 >> ' . $this->app->getPath('@var/schedule_out.txt') . ' &';
+        foreach ($entry['options'] ?? [] as $k => $v) {
+            $cmd[] = '--' . $k . '=' . (is_array($v) ? implode(',', $v) : $v);
+        }
+        $cmd[] = '>> ' . $this->app->getPath('@var/schedule_out.txt') . ' 2>&1 &';
         return implode(' ', $cmd);
     }
 
---
+++ Events/SchedulerDone.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Skvn\Event\Events;
+
+use Skvn\Event\Event as BaseEvent;
+
+
+/**
+ * Class SchedulerDone
+ * @package Skvn\Event\Events
+ *
+ * @property \Skvn\App\Application $app
+ */
+class SchedulerDone extends BaseEvent
+{
+
+}
